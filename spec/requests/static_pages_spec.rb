805877dcf04dc4cfda9448bd55ee2e4388a73ce4
whediger <wes.hediger@gmail.com>
2015-02-17 21:16:14 -0700

    static pages extra credit final

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "Static pages" do
   
+  let(:base_title) { "Ruby on Rails Tutorial Sample App |"}
   
   describe "Home page" do
     it "should have the content 'Sample App'" do
@@ -10,7 +11,7 @@ describe "Static pages" do
     end
     it "should have the right title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+      page.should have_selector('title', :text => "#{base_title} Home")
     end
   end
   
@@ -21,7 +22,7 @@ describe "Static pages" do
     end
     it "should have the right title" do
       visit '/static_pages/help'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+      page.should have_selector('title', :text => "#{base_title} Help")
     end
   end
   
@@ -32,7 +33,7 @@ describe "Static pages" do
     end
     it "should have the right title" do
       visit '/static_pages/about'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About")
+      page.should have_selector('title', :text => "#{base_title} About")
     end
   end
   
@@ -43,7 +44,7 @@ describe "Static pages" do
     end
     it "should have the right title" do
       visit '/static_pages/contact'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact")
+      page.should have_selector('title', :text => "#{base_title} Contact")
     end
   end
 end
