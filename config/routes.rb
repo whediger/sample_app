da0d385e299cebbdec03fd54602255400cbc26d3
whediger <wes.hediger@gmail.com>
2015-04-17 23:28:25 -0600

    add layout links

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'static_pages/contact'
 
-  get 'static_pages/home'
-
-  get 'static_pages/help'
+  root to: 'static_pages#home'
   
-  get 'static_pages/about'
+  #match '/contact', to: 'static_pages#contact'
+  get '/contact', to: 'static_pages#contact'
+  
+  #match '/help', to: 'static_pages#help'
+  get '/help', to: 'static_pages#help'
+  
+  #match '/about', to: 'static_pages#about'
+  get '/about', to: 'static_pages#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
